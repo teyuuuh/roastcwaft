535d81cb704ff11466aeb8ff91f8d1683c32b700
teyamawi <teyaaaatawagon@gmail.com>
2025-10-19 14:50:03 +0800

    Initial commit

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-// CORRECT Brevo import for CommonJS module
-import brevo from '@getbrevo/brevo';
+import * as brevo from '@getbrevo/brevo';
 
 dotenv.config();
 
